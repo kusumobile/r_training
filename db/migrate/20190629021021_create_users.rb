beccc66a6aaa03a6f6df79467c6f1869b2bed818
Ryotaro Kusumoto <kusumobile@icloud.com>
2019-06-30 00:40:37 +0900

    first commit

--- db/migrate/20190629021021_create_users.rb
+++ db/migrate/20190629021021_create_users.rb
@@ -1,6 +1,6 @@
-class CreateArticles < ActiveRecord::Migration[5.2]
+class CreateUsers < ActiveRecord::Migration[5.2]
   def change
-    create_table :articles do |t|
+    create_table :users do |t|
       t.string "username"
       t.string "email"
       t.datetime "created_at", null: false
